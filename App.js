9abbb6b00f6a47ba8913fbaa3acf6d84ef02e7b1
Lawal Rashidi <lawalrashidi95@gmail.com>
2023-12-01 17:07:14 +0100

    first commit

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     git
+      <Text>Welcome back to Mobile Development, Rashidi!</Text>
+      <StatusBar style="auto" />     
     </View>
   );
 }
