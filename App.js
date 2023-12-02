513de55230b5e00bf95436d38293967f820a789f
Lawal Rashidi <lawalrashidi95@gmail.com>
2023-12-02 11:49:31 +0100

    WIP: install dependencies

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { PaperProvider } from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-     
-      <Text>Welcome back to Mobile Development, Rashidi!</Text>
-      <StatusBar style="auto" />     
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Welcome back to Mobile Development, Rashidi!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
