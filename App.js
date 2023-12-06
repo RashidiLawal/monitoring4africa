5a42ab30384d8df519dee7fe476f4f214893cb9d
Lawal Rashidi <lawalrashidi95@gmail.com>
2023-12-06 15:56:17 +0100

    WIP: updated version

--- App.js
+++ App.js
@@ -1,27 +1,24 @@
-import React,{useState,useEffect} from 'react'
-// import * as Font from 'expo-font';
-// import 'react-native-gesture-handler';
-import {Provider as StoreProvider} from 'react-redux';
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
-import rootReducer from './src/store/reducers' 
-import thunk from 'redux-thunk';
-import Entry from './entry';
+import "react-native-gesture-handler";
+import React, { useState, useEffect } from "react";
+import { Provider as StoreProvider } from "react-redux";
+import { PaperProvider } from "react-native-paper";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import rootReducer from "./src/store/reducers";
+import thunk from "redux-thunk";
+import Entry from "./entry";
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk)
-);
-
-const App = () =>  { 
+const App = () => {
   return (
     <>
-    <StoreProvider store={store}>
-      <Entry />
-    </StoreProvider>
+      <StoreProvider store={store}>
+        <PaperProvider>
+          <Entry />
+        </PaperProvider>
+      </StoreProvider>
     </>
   );
 };
-
 
 export default App;
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,13 +1,9 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import {
-  NavigationContainer,
-} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
-import {
-  Provider as PaperProvider,
-} from "react-native-paper";
+import { Provider as PaperProvider } from "react-native-paper";
 import Login from "../screens/onboarding/Login";
 import Onboarding from "../screens/onboarding/onboarding";
 
@@ -18,29 +14,28 @@ const Navigation = (props) => {
   const navigationRef = React.useRef();
 
   return (
-      <PaperProvider >
-        <NavigationContainer  ref={navigationRef}>
-          <Stack.Navigator
-            // initialRouteName={
-            //   props.profile?.email ? "Home" : "Onboarding"
-            // }
-          >
-            <Stack.Screen
-              name="Onboarding"
-              component={Onboarding}
-              options={{ headerShown: false }}
-            />
-           
-            <Stack.Screen
-              name="Login"
-              component={Login}
-              options={{ headerShown: false }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </PaperProvider>
+    <PaperProvider>
+      <NavigationContainer ref={navigationRef}>
+        <Stack.Navigator
+        // initialRouteName={
+        //   props.profile?.email ? "Home" : "Onboarding"
+        // }
+        >
+          <Stack.Screen
+            name="Onboarding"
+            component={Onboarding}
+            options={{ headerShown: true }}
+          />
+
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: true }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
- 
 
 export default Navigation;
