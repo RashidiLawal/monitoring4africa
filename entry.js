25081adcf182a6672c6423c158339003ceaffefe
habeeb <habibmail31@gmail.com>
2023-12-09 11:24:59 +0100

    more update

--- entry.js
+++ entry.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { Image, Platform } from "react-native"
 import { connect, useDispatch, useSelector } from 'react-redux';
 import Navigation from './src/navigation';
@@ -6,12 +6,32 @@ import CustomView from './src/components/ui/CustomView';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { refreshProfile } from './src/store/actions/profile';
 import { refreshUserToken } from './src/store/actions/login';
+import { useFonts } from 'expo-font';
+let customFonts = {
+  'avenir-regular': require('./assets/fonts/avenir-regular.ttf'),
+  'avenir-semibold': require('./assets/fonts/avenir-semibold.ttf'),
+}
 const Entry = () => {
   const profile = useSelector(s => s.profile)
   const dispatch = useDispatch()
   const [loaded, setLoaded] = useState(false)
-    useEffect(() => {
- 
+  const [fontsLoaded, fontError] = useFonts({
+    'avenir-regular': require('./assets/fonts/avenir-regular.ttf'),
+    'avenir-semibold': require('./assets/fonts/avenir-semibold.ttf'),
+  });
+
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded || fontError) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded, fontError]);
+console.log(fontsLoaded)
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+
+    useEffect(() => { 
     AsyncStorage.getItem('userData', (err, result) => {
       if (result) {
         var userData = JSON.parse(result);
@@ -22,11 +42,11 @@ const Entry = () => {
     });
   }, [])
 
-  if (!loaded) {
-    return (<CustomView center middle flex={true} flexGrow    >
-      {/* <Image style={{ width: 50 + '%', height: 50 }} source={require("./assets/images/splashscreen.png")} /> */}
-    </CustomView>)
+
+  if (!fontsLoaded) {
+    return null;
   }
+console.log(fontsLoaded)
   return (
     <>
       <Navigation profile={profile} />
