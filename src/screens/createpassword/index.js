d5fef0c70ba05137565a26f7b5684e3053ccd53b
Lawal Rashidi <lawalrashidi95@gmail.com>
2023-12-11 12:39:22 +0100

    WIP: updated version

--- src/screens/createpassword/index.js
+++ src/screens/createpassword/index.js
@@ -11,72 +11,81 @@ import Cancel from "../../../assets/svgs/X.svg";
 import { useNavigation } from "@react-navigation/native";
 import { COLORS } from "../../store/constant/theme";
 
-
 const CreatePassword = () => {
   const navigation = useNavigation();
   return (
     <>
       <Appbar.Header style={{ backgroundColor: "#fff" }}>
-      <CustomView padding={[0, 0, 0, 5]}>
+        <CustomView padding={[0, 0, 0, 5]}>
           <Pressable onPress={() => navigation.goBack()} shadow>
             <BackIcon />
           </Pressable>
         </CustomView>
       </Appbar.Header>
       <ScrollView style={styles.container}>
-        <CustomView>
-          <CustomView>
-            <CustomText size={27} heavier color={COLORS.lightBlack} spacing={.5}>
-              Create Your Password
-            </CustomText>
-            <CustomText size={15} color={COLORS.midGrey} spacing={.5} height={18}>
-              Choose a strong password that you can remember
-            </CustomText>
+        <CustomView column rowGap={17}>
+          <CustomView column rowGap={13}>
+          <CustomText size={27} heavier color={COLORS.lightBlack} spacing={0.5}>
+            Create Your Password
+          </CustomText>
+          
+            <CustomView column>
+              <CustomText
+                size={14.5}
+                color={COLORS.midGrey}
+                spacing={0.5}
+                height={18}
+              >
+                Choose a strong password that you can
+              </CustomText>
+              <CustomText
+                size={14.5}
+                color={COLORS.midGrey}
+                spacing={0.5}
+                height={18}
+              >
+                remember
+              </CustomText>
+            </CustomView>
           </CustomView>
-          <CustomInput
-           label='Password'
-           placeholder='*************'
-          />
+          <CustomInput label="Password" placeholder="*************" />
         </CustomView>
         <CustomView column>
           <CustomView row>
-            <CustomView row center>
+            <CustomView row center columnGap={3}>
               <GreenCheck />
               <CustomText size={14}>Has at least 8 characters</CustomText>
             </CustomView>
-            <CustomView row center>
+            <CustomView row center columnGap={3}>
               <GreenCheck />
               <CustomText size={14}>Has an uppercase letter</CustomText>
             </CustomView>
           </CustomView>
           <CustomView row>
-            <CustomView row center>
+            <CustomView row center columnGap={3}>
               <Cancel />
               <CustomText size={14}>Has a number</CustomText>
             </CustomView>
-            <CustomView row center>
+            <CustomView row center columnGap={3}>
               <Cancel />
               <CustomText size={14}>Has a symbol</CustomText>
             </CustomView>
           </CustomView>
         </CustomView>
         <CustomView>
+          <CustomInput label="Confirm Password" placeholder="*************" />
           <CustomInput
-          label='Confirm Password'
-          placeholder='*************'
-          />
-          <CustomInput
-          label='Referral Code (Optional)'
-          placeholder='Enter referral code if any'
+            label="Referral Code (Optional)"
+            placeholder="Enter referral code if any"
           />
         </CustomView>
       </ScrollView>
       <CustomView padding={[20, 20, 35]} white>
-      <CustomButton onPress={() => navigation.navigate("VerifyEmail")}>
+        <CustomButton onPress={() => navigation.navigate("VerifyEmail")}>
           <CustomText white bold size={18}>
             Proceed
           </CustomText>
-      </CustomButton>
+        </CustomButton>
       </CustomView>
     </>
   );
@@ -85,23 +94,11 @@ const CreatePassword = () => {
 export default CreatePassword;
 
 const styles = StyleSheet.create({
-    body: {
-      justifyContent: "space-between",
-      height: "100%",
-  
-      backgroundColor: "#FFFFFF",
-      paddingHorizontal: 15,
-      paddingTop: 10,
-      paddingBottom: 95,
-    },
-    container: {
-      backgroundColor: "#FFF",
-      flexDirection: "column",
-      height: "100%",
-      paddingHorizontal: 15,
-      paddingTop: 5,
-  
-      // paddingBottom: 95,
-    },
-  });
-  
+  container: {
+    backgroundColor: "#FFF",
+    flexDirection: "column",
+    height: "100%",
+    paddingHorizontal: 15,
+    paddingTop: 5,
+  },
+});
