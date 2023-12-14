4b0393fc82d93ed0e2b965a085da471b39d902ec
Lawal Rashidi <lawalrashidi95@gmail.com>
2023-12-14 16:53:20 +0100

    WIP: updated version

--- src/components/Avatar/AvatarLight.js
+++ src/components/Avatar/AvatarLight.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Avatar from "./Avatar";
+// import Avatar from "./Avatar";
 import CustomView from "../ui/CustomView";
 import CustomText from "../ui/CustomText";
 import WoMan from "../../../assets/svgs/Ellipse Female.svg";
 import Man from "../../../assets/svgs/Ellipse Man.svg";
 import { COLORS } from "../../store/constant/theme";
-import { StyleSheet } from 'react-native';
+import { Image,StyleSheet } from 'react-native';
+// import {  StyleSheet } from 'react-native';
+
 
 
 const Avatar = ({imageUrl}) => {
@@ -13,7 +15,7 @@ const Avatar = ({imageUrl}) => {
    
     return (
       <CustomView style={styles.avatarContainer}>
-        <Image source={{uri:imageUrl}} style={styles.avatarImage} />
+        <Image source={{uri:'https://reactnative.dev/img/tiny_logo.png',}} style={styles.avatarImage} />
       </CustomView>
     );
   };
@@ -33,31 +35,31 @@ const AvatarList = () => {
     //     url: <CustomText color={COLORS.Test2}>HECTOR</CustomText>,
     // },
     {
-        avatarUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fpassport-photo-silhouette&psig=AOvVaw1x0-YULWyBDQFZn89xgmZY&ust=1702648667925000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCNDf2u-Kj4MDFQAAAAAdAAAAABAE",
+        avatarUrl: {uri:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fpassport-photo-silhouette&psig=AOvVaw1x0-YULWyBDQFZn89xgmZY&ust=1702648667925000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCNDf2u-Kj4MDFQAAAAAdAAAAABAE"},
     },
     {
-        avatarUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fpassport-photo-icon&psig=AOvVaw1x0-YULWyBDQFZn89xgmZY&ust=1702648667925000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCNDf2u-Kj4MDFQAAAAAdAAAAABAJ",
+        avatarUrl: {uri:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fpassport-photo-icon&psig=AOvVaw1x0-YULWyBDQFZn89xgmZY&ust=1702648667925000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCNDf2u-Kj4MDFQAAAAAdAAAAABAJ"},
     },
     {
-        avatarUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vector%2Fpassport-icon-with-female-person-profile-avatar-vector-20598862&psig=AOvVaw1x0-YULWyBDQFZn89xgmZY&ust=1702648667925000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCNDf2u-Kj4MDFQAAAAAdAAAAABAQ",
+        avatarUrl: {uri:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vector%2Fpassport-icon-with-female-person-profile-avatar-vector-20598862&psig=AOvVaw1x0-YULWyBDQFZn89xgmZY&ust=1702648667925000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCNDf2u-Kj4MDFQAAAAAdAAAAABAQ"},
     },
     {
-        avatarUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcompany-employee-id-photo-driver-licence-social-media-avatar-passport-design-element_31530188.htm&psig=AOvVaw1x0-YULWyBDQFZn89xgmZY&ust=1702648667925000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCLie29KNj4MDFQAAAAAdAAAAABAR",
+        avatarUrl: {uri:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcompany-employee-id-photo-driver-licence-social-media-avatar-passport-design-element_31530188.htm&psig=AOvVaw1x0-YULWyBDQFZn89xgmZY&ust=1702648667925000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCLie29KNj4MDFQAAAAAdAAAAABAR"},
     },
     {
-        avatarUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vector%2Fcute-young-man-avatar-character-cartoon-style-vector-36211784&psig=AOvVaw0PlJwpKuHD_LFwrd8fgt7m&ust=1702649515398000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLDv0PKNj4MDFQAAAAAdAAAAABAD",
+        avatarUrl: {uri:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vector%2Fcute-young-man-avatar-character-cartoon-style-vector-36211784&psig=AOvVaw0PlJwpKuHD_LFwrd8fgt7m&ust=1702649515398000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLDv0PKNj4MDFQAAAAAdAAAAABAD"},
     },
     {
-        avatarUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Favatar%2F&psig=AOvVaw0PlJwpKuHD_LFwrd8fgt7m&ust=1702649515398000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLDv0PKNj4MDFQAAAAAdAAAAABAI",
+        avatarUrl: {uri:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Favatar%2F&psig=AOvVaw0PlJwpKuHD_LFwrd8fgt7m&ust=1702649515398000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLDv0PKNj4MDFQAAAAAdAAAAABAI"},
     },
     {
-        avatarUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpngtree.com%2Ffreepng%2Fwoman-passport-photo-to-register-marriage-illustration_7267240.html&psig=AOvVaw0PlJwpKuHD_LFwrd8fgt7m&ust=1702649515398000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLDv0PKNj4MDFQAAAAAdAAAAABAQ",
+        avatarUrl: {uri:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fpngtree.com%2Ffreepng%2Fwoman-passport-photo-to-register-marriage-illustration_7267240.html&psig=AOvVaw0PlJwpKuHD_LFwrd8fgt7m&ust=1702649515398000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLDv0PKNj4MDFQAAAAAdAAAAABAQ"},
     },
     {
-        avatarUrl: "https://play.google.com/store/apps/details?id=com.blackgd.passportphoto",
+        avatarUrl: {uri:"https://play.google.com/store/apps/details?id=com.blackgd.passportphoto"},
     },
     {
-        avatarUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.123rf.com%2Fclipart-vector%2Fkevin_stitt.html&psig=AOvVaw0PlJwpKuHD_LFwrd8fgt7m&ust=1702649515398000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLDv0PKNj4MDFQAAAAAdAAAAABAo",
+        avatarUrl: {uri:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.123rf.com%2Fclipart-vector%2Fkevin_stitt.html&psig=AOvVaw0PlJwpKuHD_LFwrd8fgt7m&ust=1702649515398000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLDv0PKNj4MDFQAAAAAdAAAAABAo"},
     },
   ];
   return (
